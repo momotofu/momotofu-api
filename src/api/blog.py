5df53888f20047bd7796a982abdc39fd28e8db3b
Christopher Reece <mrkita.reece@gmail.com>
2018-09-06 17:46:08 +0900

    added new reciveForm api endpoint

--- src/api/blog.py
+++ src/api/blog.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint
-from flask_restful import Api, Resource, url_for
+from flask_restful import Api, Resource, url_for, reqparse
 
 blog_bp = Blueprint('blog', __name__)
 api = Api(blog_bp)
@@ -8,4 +8,13 @@ class HelloWorld(Resource):
     def get(self, id=1):
         return { 'hello' : 'world %s ' % id }
 
+class ContactForm():
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str)
+        parser.add_argument('email', type=str)
+        parser.add_argument('message', type=str)
+        args = parser.parse_args()
+
+api.add_resource(ContactForm, '/recieveForm')
 api.add_resource(HelloWorld, '/', '/hello/<int:id>')
