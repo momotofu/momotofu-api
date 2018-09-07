7e3804985ec9aaa1ca097f4a6419e8ec6e271ff1
Christopher Reece <mrkita.reece@gmail.com>
2018-09-07 18:29:57 +0900

    implemented yagmail

--- src/api/blog.py
+++ src/api/blog.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, request
 from flask_restful import Api, Resource, url_for, reqparse
+import os
 
 blog_bp = Blueprint('blog', __name__)
 api = Api(blog_bp)
@@ -9,13 +10,20 @@ class HelloWorld(Resource):
         return { 'hello' : 'world %s ' % id }
 
 class ContactForm(Resource):
+
     def post(self):
+        import yagmail
+        if 'EMAIL' in os.environ.keys() and 'EMAIL_PW' in os.environ.keys():
+            yag = yagmail.SMTP(os.environ['EMAIL'], os.environ['EMAIL_PW'])
+        else:
+            yag = yagmail.SMTP()
+
         json = request.get_json(force=True)
+        contents = [json['name'], json['message']]
+        yag.send('mrkita.reece@gmail.com', 'FROM BLOG: %s' % json['email'],
+                contents)
 
         return json, 201, {'Access-Control-Allow-Origin': '*'}
-
-
-
 
 
 api.add_resource(ContactForm, '/recieveForm')
