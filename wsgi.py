6fc0ca872b1b3ca5d7d5e7fd22b7d8819153e6d9
Christopher Reece <mrkita.reece@gmail.com>
2018-09-07 18:44:32 +0900

    fixed deployment bug

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,12 @@ from src.app import create_app
 from src.utils.utils import get_cli_args
 import os
 
-args = get_cli_args()
-
-if 'dev' in args.keys():
-    app = create_app('dev')
-else:
+if os.getenv('FLASK_CONFIG'):
     app = create_app(os.getenv('FLASK_CONFIG'))
+else:
+    args = get_cli_args()
+    if 'dev' in args.keys():
+        app = create_app('dev')
 
 if __name__ == '__main__':
     app.run()
