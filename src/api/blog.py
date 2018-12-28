39efd7d0b174a69520764b9311a291c0a6179450
Christopher Reece <mrkita.reece@gmail.com>
2018-12-29 06:41:53 +0900

    changed email to mailgun

--- src/api/blog.py
+++ src/api/blog.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, request, make_response
 from flask_restful import Api, Resource, url_for, reqparse
+import requests
 import os, json
 
 blog_bp = Blueprint('blog', __name__)
@@ -14,18 +15,16 @@ class HelloWorld(Resource):
 
 class ContactForm(Resource):
     def post(self):
-        import yagmail
-        if os.getenv('EMAIL') and os.getenv('EMAIL_PW'):
-            yag = yagmail.SMTP(os.getenv('EMAIL'), os.getenv('EMAIL_PW'))
-        else:
-            yag = yagmail.SMTP()
-
         json = request.get_json(force=True)
-        contents = [json['name'], json['message']]
-        yag.send('mrkita.reece@gmail.com', 'FROM BLOG: %s' % json['email'],
-                contents)
+        response = requests.post(
+            "https://api.mailgun.net/v3/%s/messages" % os.getenv('EMAIL_DOMAIN'),
+            auth=("api", os.getenv('EMAIL_API_KEY')),
+            data={"from": "FROM BLOG: <%s>" % json['email'],
+                  "to": [os.getenv('EMAIL_RECIPIENT')],
+                  "subject": "Message from %s" % json['name'],
+                  "text": json['message']})
 
-        return 201, {'Access-Control-Allow-Origin': '*'}
+        return response.status_code, {'Access-Control-Allow-Origin': '*'}
 
 
 api.add_resource(ContactForm, '/receiveForm')
